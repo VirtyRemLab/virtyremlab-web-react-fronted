b332a133785906c65c9c520068aae31d1fd8d952
Diego <diegogarcia@isa.uniovi.es>
2025-07-22 21:24:58 +0200

    minor changes in menu config

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { Routes, Route } from 'react-router-dom'
 import ResponsiveDrawer from './ResponsiveDrawer'
+
 import Teoria from './pages/Teoria'
 import Laboratorios from './pages/Laboratorios'
 import Home from "./pages/Home"
 import Camaras from "./pages/Camaras"
-
 import MarkdownComponent from "./pages/Markdown"
+import Simulaciones from "./pages/Simulaciones"
 
 
 
@@ -14,11 +15,13 @@ export default function App() {
     
     <Routes>
       <Route path="/" element={<ResponsiveDrawer />} >
+      
         <Route index element={<Home />} />
         <Route path="teoria" element={<Teoria />} />
         <Route path="laboratorios" element={<Laboratorios />} />
+        <Route path="simulaciones" element={<Simulaciones />} />
         <Route path="markdown" element={<MarkdownComponent />} />
-        <Route path="camaras" element={<Camaras />} />
+        <Route path="camaras" element={<Camaras />} /> 
       </Route>
     </Routes>
         
--- src/menuConfig.jsx
+++ src/menuConfig.jsx
@@ -1,9 +1,19 @@
+import Teoria from './pages/Teoria'
+import Laboratorios from './pages/Laboratorios'
+import Home from "./pages/Home"
+import Camaras from "./pages/Camaras"
+import MarkdownComponent from "./pages/Markdown"
+
+
+
 export const menuItems = [
     { text: '🏠 Inicio', path: '/' },
     { text: '📒 Teoría', path: '/teoria' },
     { text: '🧪 Laboratorios', path: '/laboratorios' },
-    { text: '📒 Markdown', path: '/markdown' },
+    { text: '📈 Simulaciones', path: '/simulaciones' },
     { text: '🎥 Cámaras', path: '/camaras' },
+    { text: '📒 Markdown', path: '/markdown' },
+    
 ];
 
 export const drawerWidth = 200;
