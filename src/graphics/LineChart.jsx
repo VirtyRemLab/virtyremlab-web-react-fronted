1b1049d7087912d13baf5c7c9b0b8ce293237398
Diego <diegogarcia@isa.uniovi.es>
2025-07-23 13:19:07 +0200

    varias lineas, colores y leyenda en LineChart

--- src/graphics/LineChart.jsx
+++ src/graphics/LineChart.jsx
@@ -10,16 +10,36 @@ import ReactECharts  from 'echarts-for-react';
 //     width: el ancho de la gráfica
 
 
-export default function LineChart({ title, x ,  y ,  height,width} ) {
-
-
+export default function LineChart({ title, x, y, height, width, colors,labels }) {
+  
+  const default_colors = ['#8B5CF6','#10B981','#FACC15']
+  if (!colors)
+    colors = default_colors;
+  if (!labels)
+    labels = y.map((item,idx)=>" ")
+  
+  const series = y.map((item,idx) => ({
+                                      data: item,
+                                      name: labels[idx],
+                                      type: 'line',
+                                      showSymbol: false,
+                                      smooth: false,
+                                      color:colors[idx]
+                                    }))
   const option = {
     title: {
       text: title,
       textStyle: {
-        color: '#ffffff', // color del título
-        fontSize: 20,
-        fontWeight: 'bold',
+        color: '#AAAAAA', // color del título
+        fontSize: 15,
+        //fontWeight: 'bold',
+      },
+    },
+    legend: {
+      textStyle: {
+        color: '#AAAAAA', // color del título
+        fontSize: 10,
+        //fontWeight: 'bold',
       },
     },
     grid: {
@@ -27,33 +47,21 @@ export default function LineChart({ title, x ,  y ,  height,width} ) {
       right: '10%',
       top: 60,
       bottom: 60,
-      containLabel: true,
-      backgroundColor: '#000000', // ⚙️ solo visible si usas un tema custom
+      //containLabel: true,
+      //backgroundColor: '#000000', // ⚙️ solo visible si usas un tema custom
     },
     tooltip: {
       trigger: 'axis',
     },
     xAxis: {
       type: 'category',
-        data: x ,
+      data: x,
+        
     },
     yAxis: {
       type: 'value',
     },
-    series: [
-      {
-            data: y ,
-        type: 'line',
-        showSymbol: false,
-        smooth: true,
-      },
-      {
-        data: y ,
-    type: 'line',
-    showSymbol: false,
-    smooth: true,
-  },
-    ],
+    series: series,
   };
 
   return <ReactECharts option={option} style={{ height: height, width: width}} />;
