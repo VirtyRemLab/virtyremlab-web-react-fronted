b9801b423e19ee641a03dc8b0db448410593400b
Diego <diegogarcia@isa.uniovi.es>
2025-07-28 15:19:52 +0200

    ghost en svg aeropendulo

--- src/graphics/aeropendulo.jsx
+++ src/graphics/aeropendulo.jsx
@@ -1,5 +1,5 @@
 
-const Aeropendulo_svg = ({angle=0}) => (
+const Aeropendulo_svg = ({angle=0,angle_ghost=0}) => (
     <svg
         // Quito el ancho y el alto para hacerlo reactivo 
     // width={365}
@@ -9,7 +9,24 @@ const Aeropendulo_svg = ({angle=0}) => (
     xmlns="http://www.w3.org/2000/svg"
     
   >
-    
+    <g id="Barra_motores_ghost" transform={` translate(0 180) rotate(${angle_ghost} 182.5 16)`}>
+      opacity={0.3}
+      <g id="Rectangle 1 ghost">
+        <rect x={0.5} y={9.5} width={364} height={14} rx={7} fill="#C059FD" fillOpacity={0.3}/>
+        <rect x={0.5} y={9.5} width={364} height={14} rx={7} stroke="#C059FD" strokeOpacity={0.5} />
+        <rect
+          x={0.5}
+          y={9.5}
+          width={364}
+          height={14}
+          rx={7}
+          stroke="#C059FD"
+          strokeOpacity={0.2}
+        />
+      </g>
+      <rect id="Rectangle 2 ghost" x={7} width={41} height={9} fill="#D9D9D9" fillOpacity={0.3} />
+      <rect id="Rectangle 3 ghost" x={318} width={41} height={9} fill="#D9D9D9" fillOpacity={0.3}/>
+    </g>
     <g id="Barra_motores" transform={` translate(0 180) rotate(${angle} 182.5 16)`}>
       <g id="Rectangle 1">
         <rect x={0.5} y={9.5} width={364} height={14} rx={7} fill="#C059FD" />
