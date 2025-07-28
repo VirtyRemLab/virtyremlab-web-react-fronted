36e39a9fa30344904e7c366fa987248e0af8f35f
Diego <diegogarcia@isa.uniovi.es>
2025-07-28 15:21:18 +0200

    PI y perturbaciones en aeropendulo

--- src/js_sim/models.js
+++ src/js_sim/models.js
@@ -3,7 +3,13 @@ import { multiply,add } from 'mathjs';
 
 // Constantes del modelos EE del aeropéndulo
 // Se identifica la función de tran
-export const aeropendulo = { A: [[-0.607, 0], [1, 0]], B: [[1], [0]], C: [[0, 0.853], [0.853, 0]], D: 0 };
+export const aeropendulo = { A: [[-0.607, 0], [1, 0]], B: [[1,1], [0,0]], C: [[0, 0.853], [0.853, 0]], D: 0 };
+export const PI = function (Kp, Ki) {
+    return { A: [[0]], B: [[1]], C: [[Kp*Ki]], D: Kp };
+}
+    
+    
+    
 
 // Función para el cálculo de \dot{X} e Y a partir de las matrices de estado y un estado inicial
 export class modeloEE {
@@ -11,12 +17,15 @@ export class modeloEE {
         this.x_dot = 0; this.y = 0;
         this.params = params;
         
+        // Métodos
+        this.update_params = (params) => { this.params = params };
+
         this.state = (u,x0) => {
             return add(multiply(this.params.A, x0), multiply(this.params.B, u));
         };
 
-        this.out = (x0) => {
-            return multiply(this.params.C, x0);
+        this.out = (u,x0) => {
+            return add(multiply(this.params.C, x0),multiply(this.params.D, u));
         }
 }
 };
